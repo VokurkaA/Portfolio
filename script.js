82f59be97d33eacf98ffb82d84f2afc995240a1c
Jan Zadrobílek <ondracz97@gmail.com>
2024-02-14 11:17:14 +0100

    inializing repository

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", (event) => {
+    const gallery = document.querySelector("#Gallery > div > div").querySelectorAll("img")
+
+    gallery.forEach(image => {
+        const path = image.getAttribute("src").split('/');
+        if (path.length == 3) {
+            path[1] = "full_rez"
+            image.src = path.join('/');
+        }
+    });
+});
